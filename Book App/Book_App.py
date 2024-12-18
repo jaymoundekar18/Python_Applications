9bd55b299462b97dc15ac6a1cad153608fdf3e79
Jay M <60815668+jaymoundekar18@users.noreply.github.com>
2024-12-18 16:46:25 +0530

    Update Book_App.py

--- Book App/Book_App.py
+++ Book App/Book_App.py
@@ -55,7 +55,78 @@ class App(ct.CTk):
     self.frontregframe = ct.CTkFrame(self.main_frame, width=1200 // 2, height=700, fg_color="transparent")
 
     # ---------------------------------------------------------------------------------------------------------
+    self.main_frame.grid_columnconfigure(0, weight=1)
+    self.main_frame.grid_columnconfigure(1, weight=1)
+    self.main_frame.grid_rowconfigure(1, weight=1)
 
+
+    # Image configuration on left side
+    self.frontimglabel = ct.CTkLabel(self.frontimgframe,text="")
+    self.frontimglabel.pack(padx=20, pady=10)
+
+    image = Image.open("img/book1.jpg")
+
+    ctk_image = ct.CTkImage(light_image=image, size=(575,575))
+    
+    self.frontimglabel.configure(image=ctk_image)
+    self.frontimglabel.image = ctk_image
+
+    
+    # Login field configuration on right side
+    self.titlelabel = ct.CTkLabel(self.frontlogframe, text="Login User", fg_color="transparent",
+                                  text_color="white", font=("arial black", 32))
+    self.titlelabel.grid(row=0,column=3,padx=20, pady=20)
+           
+    self.fronttextlabel1 = ct.CTkLabel(self.frontlogframe,text="User ID    : ", fg_color="transparent",
+                                  text_color="white", font=("Palatino Linotype", 20))
+    self.fronttextlabel1.grid(row=1,column=2,padx=20, pady=20)
+
+    self.frontentrylabel1 = ct.CTkEntry(self.frontlogframe,placeholder_text="Enter Your User ID",justify="center",width=350,height=50,bg_color="transparent",fg_color="transparent",font=("arial", 16))
+    self.frontentrylabel1.grid(row=1,column=3,padx=20, pady=20)
+
+    
+    self.fronttextlabel2 = ct.CTkLabel(self.frontlogframe,text="Password    : ", fg_color="transparent",
+                                  text_color="white", font=("Palatino Linotype", 20))
+    self.fronttextlabel2.grid(row=2,column=2,padx=20, pady=20)
+
+    self.frontentrylabel2 = ct.CTkEntry(self.frontlogframe,placeholder_text="Enter Your Password",justify="center",show="*",width=350,height=50,bg_color="transparent",fg_color="transparent",font=("arial", 16))
+    self.frontentrylabel2.grid(row=2,column=3,padx=20, pady=20)
+    
+    
+    img = Image.open("img/open_eye.png")
+    self.eye_open = ct.CTkImage(light_image=img, size=(35,30))
+
+    img = Image.open("img/closed_eye.png")
+    self.eye_closed = ct.CTkImage(light_image=img, size=(35,30))
+
+    self.eyeButton = ct.CTkButton(self.frontlogframe,image=self.eye_open,corner_radius=20, compound="left",text="",width=40,height=40, command=self.show_password)
+    self.eyeButton.grid(row=2,column=4,padx=20, pady=20)
+    self.password_visible = False
+
+    img = Image.open("img/login.png")
+    self.logbtnimg = ct.CTkImage(light_image=img, dark_image=img, size=(180, 40))  
+    
+    img = Image.open("img/register.png")
+    self.regbtnimg = ct.CTkImage(light_image=img, dark_image=img, size=(180, 40))  
+    
+
+    self.login_button = ct.CTkButton(self.frontlogframe,text="", hover_color="#3d3d3d",image=self.logbtnimg,corner_radius=1000,bg_color="transparent",fg_color="transparent", width=180, height=40 ,compound="left", command=self.login)
+    self.login_button.grid(row=4,column=3,padx=20, pady=20)
+
+    self.register_button = ct.CTkButton(self.frontlogframe,text="",image=self.regbtnimg,width=180,height=40,corner_radius=1000,bg_color="transparent",fg_color="transparent",hover_color="#3d3d3d",compound="left", command=self.register)
+    self.register_button.grid(row=5,column=3,padx=20, pady=20)
+
+# ------------------------------------------------------------------------------------------------------------------------------
+
+def login(self):
+    print("login button clicked")
+
+# ------------------------------------------------------------------------------------------------------------------------------
+
+  def register(self):
+    print("register button clicked")
+    
+# ------------------------------------------------------------------------------------------------------------------------------
 
 # Main function to run the app
 
