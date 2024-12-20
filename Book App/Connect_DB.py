57553015d0fa55c11930bc5fd54d9904a2ac9975
Jay M <60815668+jaymoundekar18@users.noreply.github.com>
2024-12-20 16:22:08 +0530

    Update Connect_DB.py

--- Book App/Connect_DB.py
+++ Book App/Connect_DB.py
@@ -4,6 +4,7 @@ import sqlite3
 
 
 class DB_Connect():
+  
   # Setting up the database connection
   def __init__(self):
     self.conn = sqlite3.connect("book_users.db")
@@ -19,12 +20,18 @@ class DB_Connect():
 # Database Operations
   
   def create_user(self, fname, mail, uname, passwd):
-    self.cursor.execute("INSERT INTO user_data (fullname, email, username, password) VALUES (?,?,?,?) ",(name,mail,uname,passwd))
+    self.cursor.execute("INSERT INTO user_data (fullname, email, username, password) VALUES (?,?,?,?) ",(fname,mail,uname,passwd))
+    print("DB : User created")
 
 ## Book recored table for user
     table_name = fname.strip().replace(" ","").lower()
     query = "CREATE TABLE IF NOT EXISTS " + table_name + " (book_name TEXT NOT NULL, book_author TEXT NOT NULL, genre TEXT NOT NULL, reading_time TEXT, book_pages INTEGER NOT NULL, book_review TEXT, rating REAL)"
+    
     self.cursor.execute(query)
+
+    print("DB : Table created ", table_name)
+
+    self.conn.commit()
 
 ## Display Particular user book data
   def show_book_data(self, tablename):
@@ -39,23 +46,72 @@ class DB_Connect():
     result = self.cursor.fetchall()
     return result
 
-  def delete_record(self):
-    pass
+## Find User
+  def findUser(self,user,passwd):
+        
+        self.cursor.execute("SELECT * FROM user_data WHERE username=? AND password=?", (user, passwd))
+        result = self.cursor.fetchone()
+        print("DB : ",result)
+        return result
+  
+## Find User Book Data
+  def findUserBook(self,tablename):
+        query = "SELECT book_name FROM " + tablename
+        try:
+           self.cursor.execute(query)
+           result = self.cursor.fetchall()
+           print("DB : ",result)
+           return result
+        
+        except Exception as e:
+           print(e)
 
-  def update_record(self):
-    pass
 
+## Adding New Book to the Particular Users Table
+  def save_new_book(self, tablename, bookname, bookauthor,genre,bpages):
+    query = "INSERT INTO " + tablename + "(book_name, book_author, genre, book_pages) values (?,?,?,?)"
+
+    try:
+      self.cursor.execute(query, (bookname, bookauthor, genre, bpages))
+      print("DB : new book added")
+
+      query = "SELECT * FROM " + tablename
+      self.cursor.execute(query)
+      self.conn.commit()
+      res = self.cursor.fetchall()
+      print(res)
+
+    except Exception as e:
+      print(e)
+
+
+## Update Book Time
+  def update_book_time(self,tablename, bookname, newtime):
+    query = "UPDATE " + tablename + " SET reading_time = ? WHERE book_name = ?"
+    try : 
+      self.cursor.execute(query, (newtime,bookname ))
+      self.conn.commit()
+      print("DB : time updated")
+
+      query = "SELECT * FROM " + tablename
+      self.cursor.execute(query)
+      res = self.cursor.fetchall()
+      print(res)
+    
+    except Exception as e:
+      print(e)
+
+
+## Delete Table From Database
   def delete_table(self, tablename):
     query = "DROP TABLE IF EXISTS " + tablename.strip().replace(" ","").lower()
     try:
       self.cursor.execute(query)
-      print(f"{tablename} table deleted successfully")
+      print(f"DB : {tablename} table deleted successfully")
       
     except Exception as e:
       print(e)
-      
   
-    
 
 
 
