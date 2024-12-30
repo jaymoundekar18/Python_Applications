95dae133143cb62c11bbe5c9324477cce4f69410
Jay M <60815668+jaymoundekar18@users.noreply.github.com>
2024-12-30 15:15:56 +0530

    Update Text_Translator.py

--- Text Translator/Text_Translator.py
+++ Text Translator/Text_Translator.py
@@ -1,1 +1,19 @@
+# Import Required Libraries
 
+import customtkinter as ct
+from tkinter import messagebox
+from deep_translator import GoogleTranslator
+
+
+# Setting the app appearance and theme
+ct.set_appearance_mode("dark")
+ct.set_default_color_theme("blue")
+
+class TranslatorApp(ct.CTk):
+
+  def __init__(self):
+    pass
+
+
+if __name__=="__main__":
+  TranslatorApp().mainloop() 
